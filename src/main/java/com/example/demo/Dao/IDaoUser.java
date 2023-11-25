13759b77106ab8e130875cde057641001192441d
Yazid0Hakimi <71094777+Yazid0Hakimi@users.noreply.github.com>
2023-11-25 16:35:54 +0100

    c2

--- src/main/java/com/example/demo/Dao/IDaoUser.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Dao;
-
-import com.example.demo.Dao.Entities.User;
-
-public interface IDaoUser extends Dao<User>{
-
-//    boolean register ();
-    User Login(String email, String password);
-}
