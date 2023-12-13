55dcb9bf602e54bb372bf925cbda5c6086791d15
Yazid0Hakimi <71094777+Yazid0Hakimi@users.noreply.github.com>
2023-12-13 22:13:53 +0100

    user Implementation set

---
+++ src/main/java/com/example/demo/Service/IServiceUser.java
@@ -0,0 +1,8 @@
+package com.example.demo.Service;
+
+import com.example.demo.Dao.Entities.User;
+
+public interface IServiceUser {
+    User login() ;
+    User register();
+}
---
+++ src/main/java/com/example/demo/Service/IServiceUserImpl.java
@@ -0,0 +1,15 @@
+package com.example.demo.Service;
+
+import com.example.demo.Dao.Entities.User;
+
+public class IServiceUserImpl implements IServiceUser{
+    @Override
+    public User login() {
+        return null;
+    }
+
+    @Override
+    public User register() {
+        return null;
+    }
+}
