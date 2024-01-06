44223ff48a42434cf8ea49d1f6b2f8cd0744242f
wail-10 <koundiwail02@gmail.com>
2024-01-06 13:20:55 +0000

    update home

--- src/main/java/ma/enset/recsys/HomeController.java
+++ src/main/java/ma/enset/recsys/HomeController.java
@@ -3,9 +3,14 @@ package ma.enset.recsys;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -17,86 +22,111 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+
 import com.google.gson.Gson;
+import javafx.util.Duration;
 
 public class HomeController implements Initializable {
     @FXML private GridPane songsGrid;
-    List<Song> recommendations;
+    @FXML private ProgressIndicator spinner;
+    private List<Song> recommendations;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        recommendations = new ArrayList<>(getRecommendations());
+        animateSpinner();
+        fetchRecommendations();
+    }
+
+    private void animateSpinner() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(spinner.progressProperty(), 0)),
+                new KeyFrame(Duration.seconds(2), new KeyValue(spinner.progressProperty(), 1))
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    private void fetchRecommendations() {
+        spinner.setVisible(true);
+
+        Task<List<Song>> task = new Task<>() {
+            @Override
+            protected List<Song> call() throws Exception {
+                List<Song> songs = new ArrayList<>();
+                String requestURL = "https://api.spotify.com/v1/recommendations?seed_tracks=0c6xIDDpzE81m2q797ordA,6nTiIhLmQ3FWhvrGafw2zj&seed_genres=morrocanRap";
+
+                try {
+                    URL url = new URL(requestURL);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setRequestProperty("Authorization", "Bearer BQBrZ3Y3hoAJzQoLu2rmnev9UKNa1e76i-VEKh0Jw4XwGXTsV5YYZIbfooNyjwCOUiblZ-kQ4VAF_Mgtx8Joz3ikpyfhRardu0wR4eYW7TSAmU3qmp0");
+                    int responseCode = connection.getResponseCode();
+
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        StringBuilder response = new StringBuilder();
+                        String inputLine;
+
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+                        in.close();
+
+                        Gson gson = new Gson();
+                        JsonObject jsonResponse = gson.fromJson(response.toString(), JsonObject.class);
+                        JsonArray items = jsonResponse.getAsJsonArray("tracks");
+
+                        for (JsonElement item : items) {
+                            JsonObject musicItem = item.getAsJsonObject();
+                            JsonObject album = musicItem.getAsJsonObject("album");
+                            String name = musicItem.get("name").getAsString();
+                            JsonArray imagesArray = album.getAsJsonArray("images");
+                            String imageUrl = imagesArray.get(1).getAsJsonObject().get("url").getAsString();
+                            JsonArray artists = musicItem.getAsJsonArray("artists");
+                            String artistName = artists.get(0).getAsJsonObject().get("name").getAsString();
+
+                            Song song = new Song();
+                            song.setSongName(name);
+                            song.setArtist(artistName);
+                            song.setCoverImg(imageUrl);
+                            songs.add(song);
+                        }
+                    } else {
+                        System.out.println("HTTP Request Failed: " + responseCode);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return songs;
+            }
+        };
+
+        task.setOnSucceeded(event -> {
+            recommendations = task.getValue();
+            updateUI();
+            spinner.setVisible(false);
+        });
+
+        new Thread(task).start();
+    }
+
+    private void updateUI() {
         for (int i = 0; i < recommendations.size(); i++) {
             try {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("song.fxml"));
                 VBox songLayout = loader.load();
 
-                // Access the controller and set data for the song card
                 SongController songController = loader.getController();
                 songController.setData(recommendations.get(i));
 
-                int row = i / 4; // Calculate row index
-                int column = i % 4; // Calculate column index
+                int row = i / 4;
+                int column = i % 4;
 
                 songsGrid.add(songLayout, column, row);
             } catch (IOException e) {
                 e.printStackTrace();
-            } catch (NullPointerException ex) {
-                ex.printStackTrace();
-                // Ensure that song.fxml or songsGrid is not null
-            } catch (Exception exc) {
-                exc.printStackTrace();
-                // Catch any other potential exceptions
             }
         }
-    }
-
-
-    private List<Song> getRecommendations() {
-        String requestURL = "https://api.spotify.com/v1/recommendations?seed_tracks=0c6xIDDpzE81m2q797ordA,6nTiIhLmQ3FWhvrGafw2zj&seed_genres=morrocanRap";
-        List<Song> songs = new ArrayList<>();
-        try {
-            URL url = new URL(requestURL);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Authorization", "Bearer BQBrZ3Y3hoAJzQoLu2rmnev9UKNa1e76i-VEKh0Jw4XwGXTsV5YYZIbfooNyjwCOUiblZ-kQ4VAF_Mgtx8Joz3ikpyfhRardu0wR4eYW7TSAmU3qmp0");
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuilder response = new StringBuilder();
-                String inputLine;
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-                Gson gson = new Gson();
-                JsonObject jsonResponse = gson.fromJson(response.toString(), JsonObject.class);
-                JsonArray items = jsonResponse.getAsJsonArray("tracks");
-                for (JsonElement item : items) {
-                    JsonObject musicItem = item.getAsJsonObject();
-                    JsonObject album = musicItem.getAsJsonObject("album");
-                    String name = musicItem.get("name").getAsString();
-                    JsonArray imagesArray = album.getAsJsonArray("images");
-                    String ImageUrl = imagesArray.get(1).getAsJsonObject().get("url").getAsString();
-                    JsonArray artists = musicItem.getAsJsonArray("artists");
-                    String artistName = artists.get(0).getAsJsonObject().get("name").getAsString();
-                    Song song = new Song();
-                    song.setSongName(name);
-                    song.setArtist(artistName);
-                    song.setCoverImg(ImageUrl);
-                    songs.add(song);
-                    System.out.println("Name: " + name);
-                    System.out.println("Artist: " + artistName);
-                    System.out.println("images : " + ImageUrl);
-                    System.out.println("---------------------");
-                }
-            } else {
-                System.out.println("HTTP Request Failed: " + responseCode);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return songs;
     }
 }
